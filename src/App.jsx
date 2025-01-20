87061371a7b0c1bc0829f3e632d2d040c99587c3
Eddan <edvin.runhellen1@gmail.com>
2025-01-20 16:00:09 +0100

    Tried mapping, spread operator etc.

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,28 @@ function App() {
   const name = "Edvin"
   const age = 25
   const date = `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
+
+  const shop = { url: '/shop', text: "Shop" }
+  const home = { url: '/home', text: "Home" }
+  const links = [home, shop]
+
+  const navbarProps = {
+    logo: './logo.png',
+    links: links
+  }
+
+  const numbers = [1, 2, 3, 4, 5];
+  const listItems = numbers.map((number) =>
+    <li>{number}</li>
+  );
+
   return (
     < div className="App">
 
-      <Navbar icon={"🐺 "} logo={'/logo.png'} number="5" />
-
+      <Navbar {...navbarProps} />
+      <ul>
+        {listItems}
+      </ul>
 
       <h1>Hello {name}</h1>
       <h2>Age {age + 5}</h2>
--- src/assets/Navbar.jsx
+++ src/assets/Navbar.jsx
@@ -1,14 +1,13 @@
 import './Navbar.css'
 
-const Navbar = (props) => {
-    console.log(props)
+
+
+const Navbar = ({ logo, links }) => {
     return <div className="Navbar">
-        <img className='Navbar-logo' src={props.logo} alt="" />
+        <img className='Navbar-logo' src={logo} alt="" />
         <div className="Navbar-links">
-            <a href="">Home</a>
-            <a href="">Shop</a>
-            <a href="">{props.icon}</a>
-            <a href="">{props.number}</a>
+            <a href="{links[0].url}">{links[0].text}</a>
+            <a href="{links[1].url}">{links[1].text}</a>
         </div>
 
     </div>
